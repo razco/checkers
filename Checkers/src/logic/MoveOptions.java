b32632ba9e18ff49b265abd849c87952a11c1eb2
razco <rcm1_2@hotmail.com>
2013-02-15 19:56:31 +0300

    valid move options in checkers

---
+++ Checkers/src/logic/MoveOptions.java
@@ -0,0 +1,8 @@
+package logic;
+
+public interface MoveOptions {
+	static int oneUp_oneRight = 1;
+	static int oneUp_OneLeft = 2;
+	static int oneDown_oneLeft = -1;	//negative to 1
+	static int oneDown_oneRight = -2;	//negative to 2
+}
