4771bb7c0f70c30331496a7ba19aac50d1684b94
razco <rcm1_2@hotmail.com>
2013-02-18 01:01:31 +0300

    move options

--- Checkers/src/logic/MoveOptions.java
+++ Checkers/src/logic/MoveOptions.java
@@ -2,7 +2,11 @@ package logic;
 
 public interface MoveOptions {
 	static int oneUp_oneRight = 1;
-	static int oneUp_OneLeft = 2;
+	static int oneUp_oneLeft = 2;
+	static int twoUp_twoRight = 3;
+	static int twoUp_twoLeft = 4;
 	static int oneDown_oneLeft = -1;	//negative to 1
 	static int oneDown_oneRight = -2;	//negative to 2
+	static int twoDown_twoLeft = -3;	//negative to 3
+	static int twoDown_twoRight = -4;	//negative to 4
 }
