ac21ed8f2d5e83525bf101284d6578231556e248
razco <rcm1_2@hotmail.com>
2013-02-18 01:03:07 +0300

    Piece interface

--- Checkers/src/logic/PieceInterface.java
+++ Checkers/src/logic/PieceInterface.java
@@ -1,13 +1,14 @@
 package logic;
 
+import java.awt.Point;
+
 public interface PieceInterface {
 
-	public void getKind();//(king or normal)//gonna be .. @deprecated
+	//public void getKind();//(king or normal)//gonna be .. @deprecated	
+	public Point getPosition();//(x,y)
 
-	public void getPosition();//(x,y)
+	public int getOwnerColor();//black or white
 
-	public void getOwnerColor();//black or white
-
-	public void isMoveOk();//get move id and return true or false
+	public boolean isMoveOk(int move);//get move id and return true or false
 
 }
