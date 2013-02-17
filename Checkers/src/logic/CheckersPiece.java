3ace7604be9043044b01aad43495037eae039509
razco <rcm1_2@hotmail.com>
2013-02-18 01:03:34 +0300

    class that represents a checkers piece

--- Checkers/src/logic/CheckersPiece.java
+++ Checkers/src/logic/CheckersPiece.java
@@ -1,5 +1,33 @@
 package logic;
 
-public class CheckersPiece {
+import java.awt.Point;
+
+public class CheckersPiece implements PieceInterface{
+
+	private CheckersValidMoves pieceMoves;
+	private Point positionOnBoard;
+	private int owner;
+
+	
+	public CheckersPiece(Point startPoint, int owner)
+	{
+		this.owner = owner;
+		pieceMoves = new CheckersValidMoves(owner);
+	}
+	
+	@Override
+	public Point getPosition() {
+		return positionOnBoard.getLocation();
+	}
+
+	@Override
+	public int getOwnerColor() {	//1 = white, -1 = black
+		return owner;
+	}
+
+	@Override
+	public boolean isMoveOk(int move) {
+		return pieceMoves.has(move);
+	}
 
 }
